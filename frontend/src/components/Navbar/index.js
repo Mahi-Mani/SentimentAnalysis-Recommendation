ed130d5bc169660cc530f2dcf579477e5ea4185c
Mahi-Mani <m.mahishamani@gmail.com>
2024-10-28 15:13:46 -0700

    Add navbar

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
+import { Disclosure } from '@headlessui/react'
 
 class Navbar extends Component {
     
     render() {
         return (
-            <div className='text-3xl font-bold underline'>
-                This is a Navbar component
+     <Disclosure as="nav" className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center text-white">
+              <p>
+                Movie Reviews & Recommendation
+              </p>
             </div>
+            <div className="hidden sm:ml-6 sm:block">
+            </div>
+          </div>
+        </div>
+      </div>
+    </Disclosure>
         );
     }
 }
