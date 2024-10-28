fdcced8c8720b772b2698b3c7835990fb8a26c38
Mahi-Mani <m.mahishamani@gmail.com>
2024-10-28 15:00:05 -0700

    Add tailwindcss form

--- frontend/src/components/Form/index.js
+++ frontend/src/components/Form/index.js
@@ -1,9 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Description, Field, Input, Label } from '@headlessui/react';
+import clsx from 'clsx'
 
 class Form extends Component {
 
     render() {
-        return "This is form component"
+        return (
+            <div className="w-full max-w-md px-4">
+                <Field>
+                    <Label className="text-sm/6 font-medium text-black">Movie Name</Label>
+                    <Description className="text-sm/6 text-black/50">Enter a movie name, to see reviews.</Description>
+                    <Input
+                        className={clsx(
+                            'mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black',
+                            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25'
+                        )}
+                    />
+                </Field>
+            </div>
+        );
     }
 }
 
