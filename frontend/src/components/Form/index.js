120d40f25efefad41cc88cdbefcbc62a41c72cd6
Mahi-Mani <m.mahishamani@gmail.com>
2024-10-28 13:08:03 -0700

    Add skeleton code for navbar and form

---
+++ frontend/src/components/Form/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from React;
+
+class Form extends Component {
+
+    render() {
+        return "This is form component"
+    }
+}
+
+export default Form;
---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from React;
+
+class Navbar extends Component {
+    
+    render() {
+        return "This is navbar component";
+    }
+}
+
+export default Navbar;
