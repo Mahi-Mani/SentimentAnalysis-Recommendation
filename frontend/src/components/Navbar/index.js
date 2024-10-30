cf24f0754387e5ba7daaa7b3f24448ece592eaa4
Mahi-Mani <m.mahishamani@gmail.com>
2024-10-30 15:38:19 -0700

    Change image of jumbotron

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -6,12 +6,12 @@ class Navbar extends Component {
   render() {
     return (
       <div
-        className="relative h-[400px] overflow-hidden rounded-lg bg-[url('https://tecdn.b-cdn.net/img/new/slides/041.webp')] bg-cover bg-no-repeat p-12 text-center text-white">
+        className="relative h-[400px] overflow-hidden rounded-lg bg-[url('https://c4.wallpaperflare.com/wallpaper/862/449/162/jack-reacher-star-wars-interstellar-movie-john-wick-wallpaper-thumb.jpg')] bg-cover bg-no-repeat p-12 text-center text-white">
         <div
           className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black/60 bg-fixed">
           <div className="flex h-full items-center justify-center">
             <div className="text-white">
-              <h2 className="mb-4 text-4xl font-semibold">Movie Reviews & Recommendation</h2>
+              <h2 className="mb-4 text-4xl font-semibold">Movie Reviews & Recommendations</h2>
               <h4 className="mb-6 text-xl font-semibold">Search for a movie to begin!</h4>
               {/* <button
                 type="button"
