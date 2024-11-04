a0d6643c5903bf0f056c6e91ccb32a5c75cc98cf
Mahi-Mani <m.mahishamani@gmail.com>
2024-11-04 11:20:24 -0800

    Implemet routing pages using react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { React, Component } from 'react';
-import Navbar from './components/Navbar';
-import Form from './components/Form';
-import List from './components/List';
-import API from './utils/API';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Reviews from "./pages/Reviews";
+import NoMatch from "./pages/NoMatch";
 
-class App extends Component {
-  state = {
-    movieName: "",
-    movies: []
-  }
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleClick = event => {
-    event.preventDefault();
-    const { movieName } = this.state;
-    this.getMovieDetails();
-    console.log(movieName);
-    console.log(this.state.movies);
-  }
-
-  getMovieDetails = () => {
-    const { movieName } = this.state;
-    API.getDetails(movieName).then(res => {
-      const { results } = res.data;
-      this.setState({
-        movies: results.splice(0, 10)
-      })
-    }).catch(err => {
-      console.log(err);
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Form
-          handleInputChange={this.handleInputChange}
-          handleClick={this.handleClick}
-        />
-        {this.state.movies.length > 0 ?
-          this.state.movies.map((movie, index) => {
-            return (
-            <List
-              title={movie.title}
-              overview={movie.overview}
-              imageSrc={`http://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              key={index}
-              index={index}
-            />
-            )
-          })
-          : ""}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="home" element={<Home/>} />
+          <Route path="reviews" element={<Reviews/>} />
+          <Route path="*" element={<NoMatch/>} />
+          {/* </Route> */}
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
