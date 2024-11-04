88b29e2052f7086ed874685ad37073e94dcaf436
Mahi-Mani <m.mahishamani@gmail.com>
2024-11-04 14:41:06 -0800

    Convert reviews to a class component

--- frontend/src/pages/Reviews.js
+++ frontend/src/pages/Reviews.js
@@ -1,22 +1,29 @@
-import { React } from "react";
+import { React, Component } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Display from "../components/Display";
 
-function Reviews() {
-    const location = useLocation();
-    const { movieId } = location.state;
-    const { backdrop } = location.state;
-    console.log(movieId);
-    return (
-        <>
-            <Navbar
-                page="reviews"
-                backdrop={backdrop}
-            />
-            <Display />
-        </>
-    )
+class Reviews extends Component {
+
+    componentDidMount() {
+        const location = useLocation();
+        const { movieId } = location.state;
+        const { backdrop } = location.state;
+        console.log(movieId);
+    }
+
+    render() {
+        return
+        (
+            <>
+                <Navbar
+                    page="reviews"
+                    backdrop={backdrop}
+                />
+                <Display />
+            </>
+        )
+    }
 }
 
 export default Reviews;
