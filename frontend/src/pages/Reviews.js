97ce59bc4eaf6ea261cac8861aedd4ae4d336c96
Mahi-Mani <m.mahishamani@gmail.com>
2024-11-05 11:28:21 -0800

    Create higher order component for reviews

--- frontend/src/pages/Reviews.js
+++ frontend/src/pages/Reviews.js
@@ -1,22 +1,31 @@
-import { React } from "react";
+import { React, Component } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Display from "../components/Display";
 
-function Reviews() {
-    const location = useLocation();
-    const { movieId } = location.state;
-    const { backdrop } = location.state;
-    console.log(movieId);
-    return (
-        <>
-            <Navbar
-                page="reviews"
-                backdrop={backdrop}
-            />
-            <Display />
-        </>
-    )
+function wrapperReviews(WrapperComponent) {
+    return (props) => {
+        const location = useLocation();
+        return <WrapperComponent
+            {...props}
+            location={location}
+        />
+    }
+}
+class Reviews extends Component {
+    render() {
+        const { location } = this.props;
+        const { backdrop } = location.state;
+        return (
+            <>
+                <Navbar
+                    page="reviews"
+                    backdrop={backdrop}
+                />
+                <Display />
+            </>
+        )
+    }
 }
 
-export default Reviews;
+export default wrapperReviews(Reviews);
