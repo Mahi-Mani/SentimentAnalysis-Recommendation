da0869b8431bb816d146cdb7711ceff2854ef780
Mahi-Mani <m.mahishamani@gmail.com>
2024-11-05 11:14:57 -0800

    Convert reviews back to functional component

--- frontend/src/pages/Reviews.js
+++ frontend/src/pages/Reviews.js
@@ -1,29 +1,22 @@
-import { React, Component } from "react";
+import { React } from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Display from "../components/Display";
 
-class Reviews extends Component {
-
-    componentDidMount() {
-        const location = useLocation();
-        const { movieId } = location.state;
-        const { backdrop } = location.state;
-        console.log(movieId);
-    }
-
-    render() {
-        return
-        (
-            <>
-                <Navbar
-                    page="reviews"
-                    backdrop={backdrop}
-                />
-                <Display />
-            </>
-        )
-    }
+function Reviews() {
+    const location = useLocation();
+    const { movieId } = location.state;
+    const { backdrop } = location.state;
+    console.log(movieId);
+    return (
+        <>
+            <Navbar
+                page="reviews"
+                backdrop={backdrop}
+            />
+            <Display />
+        </>
+    )
 }
 
 export default Reviews;
